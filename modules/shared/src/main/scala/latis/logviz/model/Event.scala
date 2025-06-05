cd07981c281b0bfd2c118ee0a9b08855b657a90b
Jerry <jli20031111@gmail.com>
2025-06-05 13:25:08 -0600

    Created Log Events data model and read toy log data to be displayed onto browser

--- modules/shared/src/main/scala/latis/logviz/model/Event.scala
+++ modules/shared/src/main/scala/latis/logviz/model/Event.scala
@@ -4,17 +4,45 @@ import cats.syntax.all.*
 import io.circe.Decoder
 import io.circe.DecodingFailure
 
-enum Event {
-  case Hello
-  case World
+enum Event { //can you extend an enum? prevent
+  case Server(time: Long) 
+  case Request(time: Long, request: String)
+  case Response(time: Long, status: Int)
+  case Success(time: Long, duration: Long) 
+  case Failure(time: Long, msg: String)
 }
 
 object Event {
   given Decoder[Event] = Decoder.instance { cursor =>
-    val ev = cursor.downField("event")
+    val ev = cursor.downField("eventType")
     ev.as[String].flatMap {
-      case "hello" => Event.Hello.asRight
-      case "world" => Event.World.asRight
+      case "Server" => 
+        cursor.downField("time").as[Long].map(t => Server(t)) //map will only apply if it's a right
+      
+      case "Request" => 
+        for {
+          t <- cursor.downField("time").as[Long]
+          r <- cursor.downField("request").as[String]
+        } yield Request(t,r)
+      
+      case "Response" => 
+        for {
+          t <- cursor.downField("time").as[Long]
+          s <- cursor.downField("status").as[Int]
+        } yield Response(t, s)
+      
+      case "Success" => 
+        for {
+          t <- cursor.downField("time").as[Long]
+          d <- cursor.downField("duration").as[Long]
+        } yield Success(t, d)
+
+      case "Failure" => 
+        for {
+          t <- cursor.downField("time").as[Long]
+          m <- cursor.downField("msg").as[String]
+        } yield Failure(t, m)
+        
       case typ => DecodingFailure(s"Unknown event type: $typ", ev.history).asLeft
     }
   }
