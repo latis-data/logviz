4f816f3331025f8cedba302e56d7477d7f59ab94
Isabelle Godfrey <isgo5880>
2025-07-31 11:36:40 -0600

    Reworked code and implemented public method

--- modules/splunk/src/main/scala/latis/logviz/splunk/SplunkConfig.scala
+++ modules/splunk/src/main/scala/latis/logviz/splunk/SplunkConfig.scala
@@ -1,9 +1,11 @@
 package latis.logviz.splunk
 
+import org.http4s.Uri
 import pureconfig.ConfigReader
+import pureconfig.module.http4s.*
 
 case class SplunkConfig(
-  uri: String,
+  uri: Uri,
   username: String,
   password: String
 ) derives ConfigReader
