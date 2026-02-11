4dbbf4a432005f4840c2ac530ed95483f2fba4d5
Jerry <jli20031111@gmail.com>
2026-02-11 11:44:27 -0700

    making event request with query params

--- modules/frontend/src/main/scala/latis/logviz/EventClient.scala
+++ modules/frontend/src/main/scala/latis/logviz/EventClient.scala
@@ -12,10 +12,24 @@ import org.http4s.Uri
 import org.http4s.client.Client
 
 import latis.logviz.model.Event
+import java.time.LocalDateTime
 
 /** EventClient used to return stream of events */
 trait EventClient {
-  def getEvents: Stream[IO, Event]
+  def getEvents(startTime: String, endTime: Option[String]): Stream[IO, Event]
+
+  //no given start and end time: past 24 hours + live
+  def getEvents: Stream[IO, Event] =
+    val curr = LocalDateTime.now(java.time.ZoneId.of("UTC"))
+    val start = curr.toLocalDate().atStartOfDay().toString()
+    getEvents(start, None)
+
+  //no end time (live)
+  def getEvents(start: String): Stream[IO, Event] = 
+    getEvents(start, None)
+
+  def getEvents(start: String, end: String): Stream[IO, Event] = 
+    getEvents(start, Some(end))
 }
 
 object EventClient {
@@ -39,8 +53,26 @@ object EventClient {
       val baseUri = Uri.unsafeFromString(s"$protocol//$host")
 
       new EventClient {
-        override def getEvents: Stream[IO, Event] =
-          val request = Request[IO](method = Method.GET, uri = baseUri / "events")
+        override def getEvents(startTime: String, endTime: Option[String]): Stream[IO, Event] =         
+          
+          val uri = endTime match {
+            case None => 
+              //should there be a live query parameter?
+              // println("no end time. LIVE!!")
+              val endTime = LocalDateTime.now(java.time.ZoneId.of("UTC"))
+              val end = endTime.toString()
+              Uri.unsafeFromString(s"$baseUri/events?startTime=$startTime&endTime=$end")
+            
+            case Some(value) => 
+              // println(s"end time given! $value")
+              //for some reason it seems like we're not making a new http request when I change the end date? 
+              //changing from live to not live also doesnt seem to make a new http request, or at 
+              Uri.unsafeFromString(s"$baseUri/events?startTime=$startTime&endTime=$value")
+          }
+
+          val request = Request[IO](method = Method.GET, uri = uri)
+          // println(uri)
+          // val request = Request[IO](method = Method.GET, uri = baseUri / "events")
           
           http.stream(request).flatMap{ res =>
             res.body
