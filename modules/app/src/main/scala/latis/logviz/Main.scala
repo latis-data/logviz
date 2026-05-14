04c989ea87f0d188c514e7ed21a1629105dd059a
Isabelle Godfrey <112148392+isgo5880@users.noreply.github.com>
2026-05-14 12:31:27 -0600

    Add support for Splunk auth using token

--- modules/app/src/main/scala/latis/logviz/Main.scala
+++ modules/app/src/main/scala/latis/logviz/Main.scala
@@ -24,7 +24,8 @@ object Main extends IOApp.Simple {
     splunkConf <- Resource.eval(ConfigSource.default.at("logviz.splunk").loadF[IO, SplunkConfig]())
     source     <- splunkConf match {
       case SplunkConfig.Disabled => Resource.pure[IO, JSONEventSource](JSONEventSource())
-      case SplunkConfig.Enabled(uri, username, password, source, index) => SplunkClient.make(uri, username, password).map(client => SplunkEventSource(client, source, index))
+      case SplunkConfig.Enabled(uri, source, index, authtype) => 
+        SplunkClient.make(uri, authtype).map(client => SplunkEventSource(client, source, index))
     }
   } yield (source, source)
 
--- modules/splunk/src/main/scala/latis/logviz/splunk/SplunkConfig.scala
+++ modules/splunk/src/main/scala/latis/logviz/splunk/SplunkConfig.scala
@@ -3,17 +3,25 @@ package latis.logviz.splunk
 import cats.syntax.all.*
 import org.http4s.Uri
 import pureconfig.ConfigReader
+import pureconfig.error.*
 import pureconfig.module.http4s.*
 
 enum SplunkConfig {
   case Disabled
   case Enabled(
     uri: Uri,
-    username: String,
-    password: String,
     source: String,
-    index: String
+    index: String,
+    auth: AuthType
   )
+}
+
+enum AuthType {
+  case UserPass(
+    username: String,
+    password: String
+  )
+  case Token(token: String)
 }
 
 object SplunkConfig {
@@ -21,13 +29,24 @@ object SplunkConfig {
     c.asObjectCursor.flatMap { c => 
       c.atKey("enabled").flatMap(_.asBoolean).flatMap {
         case false => SplunkConfig.Disabled.pure[ConfigReader.Result]
-        case true => (
-          c.atKey("uri").flatMap(ConfigReader[Uri].from),
-          c.atKey("username").flatMap(_.asString),
-          c.atKey("password").flatMap(_.asString),
-          c.atKey("source").flatMap(_.asString),
-          c.atKey("index").flatMap(_.asString)
-        ).mapN(SplunkConfig.Enabled.apply)
+        case true =>
+          (
+            c.atKey("uri").flatMap(ConfigReader[Uri].from),
+            c.atKey("source").flatMap(_.asString),
+            c.atKey("index").flatMap(_.asString),
+            c.atKey("auth").flatMap(_.asString).flatMap { authString =>
+              authString.toLowerCase match {
+                case "user-pass-auth" =>
+                  (
+                    c.atKey("username").flatMap(_.asString),
+                    c.atKey("password").flatMap(_.asString)
+                  ).mapN(AuthType.UserPass.apply)
+                case "token-auth" =>
+                  (c.atKey("token").flatMap(_.asString)).map(AuthType.Token.apply)
+                case s: String => c.failed(CannotConvert(s, "AuthType", s"$s is not a valid auth type"))
+              }
+            }
+          ).mapN(SplunkConfig.Enabled.apply)
       }
     }
   }
